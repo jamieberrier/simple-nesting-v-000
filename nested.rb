3c77a12cf0582eb6264cb3f7b4f474f0b13fd79d
jamieberrier <berrier.jamie@gmail.com>
2018-08-06 00:58:35 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -93,7 +93,7 @@ def adding_matz
     }
 
 	programmer_hash
-  
+
 end
 
 def changing_alan
@@ -115,7 +115,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
 
-
+     programmer_hash
 end
 
 def adding_to_dennis
